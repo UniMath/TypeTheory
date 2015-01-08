0889fbd732a143d684115fca47d1e8fd00b646c4
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2015-01-08 15:12:17 +0100

    started file on cwfs, added emacs infrastructure file, added Makefiles

---
+++ coq/cwf.v
@@ -0,0 +1,6 @@
+
+Require Export Systems.UnicodeNotations.
+Require Export UniMath.RezkCompletion.precategories.
+
+Local Notation "a ⇒ b" := (precategory_morphisms a b)(at level 50).
+Local Notation "f □ g" := (compose f g)(at level 50).
