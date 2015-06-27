6556e277396ff8c20c535600d2d6c2f6e7bb13f0
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2015-06-27 16:50:11 +0200

    added proof that going from CwF to DM via CompCat is same as direct

---
+++ coq/CwF_to_CompCat_to_DM_structure.v
@@ -0,0 +1,44 @@
+
+(** 
+  
+ Ahrens, Lumsdaine, Voevodsky, 2015
+
+Contents:
+
+  - Construction of a Comprehension precategory from a precategory with Families
+  - Proof that the constructed CompCat is split
+
+*)
+
+
+Require Import UniMath.RezkCompletion.total2_paths.
+
+Require Import Systems.Auxiliary.
+Require Import Systems.UnicodeNotations.
+Require Import Systems.CompCat_structure.
+Require Import Systems.CwF_structure.
+Require Import Systems.CompCat_structure.
+Require Import Systems.DM_structure.
+Require Import Systems.CwF_to_CompCat_structure.
+Require Import Systems.CwF_to_DM_structure.
+Require Import Systems.CompCat_to_DM_structure.
+
+Require Import UniMath.RezkCompletion.limits.pullbacks.
+
+Section compare_maps.
+
+  Context (CC : precategory) (C : cwf_struct CC) (H : is_category CC).
+
+  Lemma maps_equal : DM_structure_of_CompCat _ H (comp_cat_of_precwf _ C (pr2 H)) = DM_structure_of_CwF _ C H.
+  Proof.
+    apply DM_equal.
+    - exact H.
+    - simpl. intros.
+      unfold DM_type in *.
+      unfold dm_sub_struct_of_CompCat in *.
+      unfold dm_sub_struct_of_CwF.
+      apply X.
+    - intros. apply X.
+  Defined.
+      
+End compare_maps.
