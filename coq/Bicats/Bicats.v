137802c3d15f22641d42433abf70768148cd5483
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2015-08-08 13:35:17 +0200

    Factored [prod_precategory] into separate lemmas, for best performance; also improved documentation of [transparent assert].

--- coq/Bicats/Bicats.v
+++ coq/Bicats/Bicats.v
@@ -16,20 +16,19 @@ Require Import Systems.UnicodeNotations.
 
 Section Background.
 
-(* Fantastically useful tactic, taken from Jason Gross and Aruand Spiwack at <https://coq.inria.fr/bugs/show_bug.cgi?id=3551>. 
+(* Very useful tactic, taken from Jason Gross and Aruand Spiwack at <https://coq.inria.fr/bugs/show_bug.cgi?id=3551>. 
 
-  Typical usage: you want to construct an instance of a big record type, where later fields depend on earlier ones, but the constructions of the earlier fields are non-trivial enough that you want to do them interactively, not write them explicitly.
+  Typical usage: you want to construct an instance of a big iterated sigma-type, where later components depend on earlier ones, but the constructions of the earlier components are non-trivial enough that you want to do them interactively, not write them explicitly.
 
-  Older versions of Coq required the earlier fields to be broken out as separate lemmas, to do this.
+  Older versions of Coq required the earlier components to be broken out as separate lemmas for this.  In current Coq, you can get this out-of-the-box with [ refine (tpair _ _ _)].  However, the resulting proof-term often becomes quite slow to work with.
 
-  Out-of-the-box in current Coq, you can get this with [ refine (Build_some_big_record _ _ _ _ _) ].  However, the resulting proof-term often becomes quite slow to typecheck.
+  Instead, you can do [ transparent assert ( H : whatever_type ). ], then build the component, and then [ exists H. ]  This can be repeated with multiple successively dependent components.
 
-  Instead, you can do [ transparent assert ( H : record_field_type ). ], then build the field instance, and then [ refine (Build_some_big_record _ _ H _ _). ]  This can be repeated with multiple successively dependent fields; typically, until all subgoals produced by [refine] are independent.  This gains a *lot* of speed for each early field factored out.
+  This gains a *lot* of speed for each early component factored out — better performance both in the current construction itself (since not so many subgoals are being simultaneously updated), and when it is used down the line (since it allows better sharing between repeated occurrences of terms).  On the latter aspect, though, even more speed can sometimes be gained by breaking the asserts out further into standalone lemmas.
 
-  It is often convenient to write the construction first using a single [ refine ], since this generates all the required field types as you go along, and then afterwards to speed up compilation by refactoring into the form with [transparent assert] for as many fields as desired. *)
+  It is often convenient to write the construction first using [ refine ], since this generates all the required field types as you go along, and then afterwards to speed up compilation by breaking fields out using [transparent assert] or as standalone lemmas. *)
 Tactic Notation "transparent" "assert" "(" ident(H) ":" open_constr(type) ")" :=
   refine (let H := (_ : type) in _).
-
 
 (* TODO: try to find this somwehre in library? *)
 Definition dirprod_paths {A B : Type} {p q : A × B}
@@ -63,18 +62,22 @@ Proof.
   (* functor_comp *) intros x y z w v; simpl. apply pathsinv0, id_left.
 Defined.
 
-Definition prod_precategory (C D : precategory) : precategory.
-Proof.
-  transparent assert (prod_precategory_ob_mor : precategory_ob_mor).
+Definition prod_precategory_ob_mor (C D : precategory) : precategory_ob_mor.
   (* ob *) exists (C × D).
   (* mor *) intros a b. refine (_ × _).
-      exact ((pr1 a) ⇒ (pr1 b)). exact ((pr2 a) ⇒ (pr2 b)).
-  transparent assert (prod_precategory_data : precategory_data).
-  exists prod_precategory_ob_mor; split.
+    exact ((pr1 a) ⇒ (pr1 b)). exact ((pr2 a) ⇒ (pr2 b)).
+Defined.
+
+Definition prod_precategory_data (C D : precategory) : precategory_data.
+  exists (prod_precategory_ob_mor C D); split.
   (* identity *) split; apply identity.
   (* comp *) intros a b c f g. split; eapply compose. 
     exact (pr1 f). exact (pr1 g). exact (pr2 f). exact (pr2 g).
-  exists prod_precategory_data.
+Defined.
+
+Definition prod_precategory (C D : precategory) : precategory.
+Proof.
+  exists (prod_precategory_data C D).
   split; try split; try split; intros.
   (* id_left *) apply dirprod_paths; simpl; apply id_left.
   (* id_right *) apply dirprod_paths; simpl; apply id_right. 
@@ -92,28 +95,13 @@ Proof.
   (* functor_on_morphisms *) intros a b; apply dirprod_assoc.
 Defined.
 
-(* TODO: next two lemmas are still rather slow (though much better now that [prod_precategory] is written using [transparent assert]).
-
-  Try (a) factoring those transparent asserts into separate lemmas; (b) grouping these lemmas into transparent asserts; see if either affects timing.  *)
-Definition prod_precategory_assoc_idax (C0 C1 C2 : precategory)
-  : functor_idax (prod_precategory_assoc_data C0 C1 C2).
-Proof.
-  (* functor_id *) intros c. Time simpl. apply paths_refl.
-Defined.
-
-Definition prod_precategory_assoc_compax (C0 C1 C2 : precategory)
-  : functor_compax (prod_precategory_assoc_data C0 C1 C2).
-Proof.
-  (* functor_comp *) intros c0 c1 c2 f g. Time simpl. apply paths_refl.
-Defined.
-
 Definition prod_precategory_assoc (C0 C1 C2 : precategory)
   : functor (C0 × (C1 × C2)) ((C0 × C1) × C2).
 Proof.
-  exists (prod_precategory_assoc_data _ _ _); split.
-  (* functor_id *) apply prod_precategory_assoc_idax.
-  (* functor_comp *) apply prod_precategory_assoc_compax.
-Time Defined.
+  exists (prod_precategory_assoc_data _ _ _). split.
+  (* functor_id *) intros c. simpl; apply paths_refl.
+  (* functor_comp *) intros c0 c1 c2 f g. simpl; apply paths_refl.
+Defined.
 
 End Background. 
 
