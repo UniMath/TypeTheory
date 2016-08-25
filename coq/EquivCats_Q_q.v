0e729582362c0dd966b182954183954a66b1a609
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2016-08-25 19:24:05 +0200

    compatible pairs category and projection functors

--- coq/EquivCats_Q_q.v
+++ coq/EquivCats_Q_q.v
@@ -4,9 +4,8 @@
 
 Main definitions:
 
-- [obj_ext_precat]
-- [families_disp_precat], [families_structure_precat]
-- [qq_structure_disp_precat], [qq_structure_precat]
+- [families_precategory]
+- [qq_structure_precategory]
 *)
 
 Require Import UniMath.CategoryTheory.limits.pullbacks.
@@ -17,11 +16,16 @@ Require Import Systems.Auxiliary.
 Require Import Systems.Structures.
 Require UniMath.Ktheory.Precategories.
 Require Import UniMath.Ktheory.StandardCategories.
-
+Require Import Systems.CwF_SplitTypeCat_Maps.
 
 Local Set Automatic Introduction.
 (* only needed since imports globally unset it *)
 
+Notation Precategory := Precategories.Precategory.
+Coercion Precategories.Precategory_to_precategory
+  : Precategories.Precategory >-> precategory.
+Notation homset_property := Precategories.homset_property.
+Notation functorPrecategory := Precategories.functorPrecategory.
 
 (** Some local notations, *)
 
@@ -32,8 +36,8 @@ Local Notation Δ := comp_ext_compare.
 
 Section fix_cat_obj_ext.
 
-Variable C : precategory.
-Variable hsC : has_homsets C.
+Variable C : Precategories.Precategory.
+Definition hsC : has_homsets C := homset_property C.
 Variable X : obj_ext_structure C.
 
 
@@ -152,59 +156,181 @@ Proof.
   exact @families_mor.
 Defined.
 
-Definition families_id_comp : disp_precat_id_comp _ families_ob_mor.
+Definition families_id_comp : precategory_id_comp families_ob_mor.
 Proof.
   apply tpair.
-  - intros X Y. simpl; unfold families_mor.
+  - intros Y. simpl; unfold families_mor.
     exists (identity _). apply tpair.
-    + etrans. apply id_left. apply pathsinv0, id_right.
-    + intros Γ A. etrans. apply id_right.
-      apply pathsinv0. refine (_ @ id_left _).
-      refine (maponpaths (fun k => k ;; _) _).
-      apply functor_id.
-  - intros X0 X1 X2 F G Y0 Y1 Y2 FF GG.
+    + apply id_left. 
+    + intros Γ A. apply id_right.
+  - intros Y0 Y1 Y2 FF GG.
     exists (families_mor_TM FF ;; families_mor_TM GG). apply tpair.
     + etrans. apply @pathsinv0. apply assoc.
       etrans. apply maponpaths, families_mor_pp.
-      etrans. apply assoc.
-      etrans. apply cancel_postcomposition, families_mor_pp.
-      apply pathsinv0. apply assoc.
+      apply families_mor_pp.
     + intros Γ A.
       etrans. apply assoc.
       etrans. apply cancel_postcomposition, families_mor_Q.
-      etrans. apply @pathsinv0, assoc.
-      etrans. apply maponpaths, families_mor_Q.
-      etrans. apply assoc.
-      apply cancel_postcomposition.
-      apply pathsinv0, functor_comp.
+      apply families_mor_Q.
 Defined.
 
-Definition families_data : disp_precat_data (obj_ext_Precat C)
+Definition families_data : precategory_data 
   := (_ ,, families_id_comp).
 
-Definition families_axioms : disp_precat_axioms _ families_data.
+Definition families_axioms : is_precategory families_data.
 Proof.
   repeat apply tpair.
   - intros. apply families_mor_eq. intros.
-    etrans. Focus 2. apply @pathsinv0, families_mor_transportf.
     apply idpath.
   - intros. apply families_mor_eq. intros.
-    etrans. Focus 2. apply @pathsinv0, families_mor_transportf.
     apply idpath.
   - intros. apply families_mor_eq. intros.
-    etrans. Focus 2. apply @pathsinv0, families_mor_transportf.
     apply idpath.
-  - intros. apply isaset_total2.
-    apply functor_category_has_homsets.
-    intros. apply isasetaprop, isapropdirprod.
-    apply functor_category_has_homsets.
-    repeat (apply impred_isaprop; intro). apply functor_category_has_homsets.
 Qed.
 
-Definition families_disp_precat : disp_precat (obj_ext_Precat C)
+
+Definition families_precategory : precategory 
   := (_ ,, families_axioms).
 
-Definition families_structure_precat : precategory
-  := total_precat families_disp_precat.
+
+Lemma has_homsets_families_precat 
+  : has_homsets families_precategory.
+Proof.
+  intros a b. apply isaset_total2.
+  apply functor_category_has_homsets.
+  intros. apply isasetaprop, isapropdirprod.
+  apply functor_category_has_homsets.
+  repeat (apply impred_isaprop; intro). apply functor_category_has_homsets.
+Qed.
 
 End Families_Structure_Precat.
+
+(** * Precategory of cartesian _q_-morphism-structures *)
+Section qq_Structure_Precat.
+
+Definition qq_structure_ob_mor : precategory_ob_mor.
+Proof.
+  exists (qq_morphism_structure X).
+  intros Z Z'.
+  refine (Π Γ' Γ (f : C ⟦ Γ' , Γ ⟧) (A : Ty X Γ), _).
+  refine (qq Z f A  = _).
+  refine (qq Z' f _ ).
+Defined.
+
+Lemma isaprop_qq_structure_mor
+  (Z Z' : qq_structure_ob_mor)
+  : isaprop (Z --> Z').
+Proof.
+  repeat (apply impred_isaprop; intro). apply hsC. 
+Qed.
+
+Definition qq_structure_id_comp : precategory_id_comp qq_structure_ob_mor.
+Proof.
+  apply tpair.
+  - intros Z; cbn.
+    intros Γ Γ' f A. apply idpath.
+(*
+    etrans. apply id_right.
+    apply pathsinv0.
+    etrans. apply @pathsinv0, assoc. 
+    etrans. apply id_left.
+    etrans.
+      apply cancel_postcomposition.
+      apply comp_ext_compare_id_general.
+    apply id_left.
+*)
+  - intros Z0 Z1 Z2.
+    intros FF GG Γ Γ' f A. cbn.
+    etrans. apply FF. apply GG.
+Qed.
+
+Definition qq_structure_data : precategory_data 
+  := (_ ,, qq_structure_id_comp).
+
+Definition qq_structure_axioms : is_precategory qq_structure_data.
+Proof.
+  repeat apply tpair; intros;
+    try apply isasetaprop; apply isaprop_qq_structure_mor.
+Qed.
+
+Definition qq_structure_precategory : precategory
+  := (_ ,, qq_structure_axioms).
+
+End qq_Structure_Precat.
+
+Arguments qq_structure_precategory : clear implicits.
+
+Section Compatible_Disp_Cat.
+
+(* TODO: rename [strucs_compat_FOO] to [strucs_iscompat_FOO] throughout, to disambiguate these from the sigma’d displayed-precat [compat_structures]. *)
+
+Definition strucs_compat_ob_mor
+  : precategory_ob_mor.
+Proof.
+  use tpair.
+  - exact (Σ YZ : (families_precategory × qq_structure_precategory), 
+                  iscompatible_fam_qq (pr1 YZ) (pr2 YZ)).
+  - 
+    intros YZ YZ'.
+    exact ((pr1 (pr1 YZ)) --> (pr1 (pr1 YZ')) × (pr2 (pr1 YZ)) --> pr2 (pr1 YZ')).
+Defined.
+
+Definition strucs_compat_id_comp
+  : precategory_id_comp strucs_compat_ob_mor.
+Proof.
+  split. 
+  - intro; split; apply identity.
+  - intros a b c f g. split. 
+    eapply @compose. apply (pr1 f). apply (pr1 g). 
+    eapply @compose. apply (pr2 f). apply (pr2 g). 
+Defined.
+
+Definition strucs_compat_data : precategory_data 
+  := ( _ ,, strucs_compat_id_comp).
+
+Definition strucs_compat_axioms : is_precategory strucs_compat_data.
+Proof.
+  repeat apply tpair; intros.
+  - apply dirprodeq;    apply id_left.
+  - apply dirprodeq; apply id_right.
+  - apply dirprodeq; apply assoc.
+Qed.
+
+Definition compat_structures_precategory
+  : precategory 
+:= ( _ ,, strucs_compat_axioms).
+
+(* should this be the name of the compatible pairs category? *)
+(*
+Definition compat_structures_disp_precat
+  := sigma_disp_precat strucs_compat_disp_precat.
+*)
+
+
+Definition compat_structures_pr1_functor
+  : functor compat_structures_precategory families_precategory.
+Proof.
+  mkpair.
+  - mkpair.
+    + exact (fun YZp => pr1 (pr1 YZp)).
+    + intros a b f. apply (pr1 f).
+  - mkpair.
+    + intro c. apply idpath.
+    + intros a b c f g. apply idpath.
+Defined.
+
+Definition compat_structures_pr2_functor
+  : functor compat_structures_precategory qq_structure_precategory.
+Proof.
+  mkpair.
+  - mkpair.
+    + exact (fun YZp => pr2 (pr1 YZp)).
+    + intros a b f. apply (pr2 f).
+  - mkpair.
+    + intro c. apply idpath.
+    + intros a b c f g. apply idpath.
+Defined.
+
+
+End Compatible_Disp_Cat.
+
