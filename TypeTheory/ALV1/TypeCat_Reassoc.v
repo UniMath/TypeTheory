d5424e7a3f01a70deb1bc461601b8c700583286e
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2017-01-11 22:52:33 -0500

    Condensed [q_etc] in structural equivalence for split type cats — big performance speedup.

--- TypeTheory/ALV1/TypeCat_Reassoc.v
+++ TypeTheory/ALV1/TypeCat_Reassoc.v
@@ -24,23 +24,21 @@ Context
   (T_ext : T_ty -> UU)
   (T_dpr : Π ty, T_ext ty -> UU)
   (T_reind : T_ty -> UU)
-  (T_q : Π ty, T_ext ty -> T_reind ty -> UU)
-  (T_dpr_q : Π ty ext reind, (T_dpr ty ext) -> (T_q ty ext reind) -> UU)
-  (T_reind_pb : Π ty ext reind dpr q, (T_dpr_q ty ext reind dpr q) -> UU)
+  (T_q_etc : Π ty ext, (T_dpr ty ext) -> (T_reind ty) -> UU)
   (T_set : T_ty -> UU)
   (T_reind_id : Π ty, T_reind ty -> UU)
-  (T_q_id : Π ty ext reind, (T_q ty ext reind) -> (T_reind_id ty reind) -> UU)
+  (T_q_id : Π ty ext dpr reind,
+    (T_q_etc ty ext dpr reind) -> (T_reind_id ty reind) -> UU)
   (T_reind_comp : Π ty, T_reind ty -> UU)
-  (T_q_comp : Π ty ext reind, (T_q ty ext reind) -> (T_reind_comp ty reind) -> UU).
+  (T_q_comp : Π ty ext dpr reind, 
+    (T_q_etc ty ext dpr reind) -> (T_reind_comp ty reind) -> UU).
 
 Arguments T_dpr [_] _.
-Arguments T_q [_] _ _.
-Arguments T_dpr_q [_ _ _] _ _.
-Arguments T_reind_pb [_ _ _ _ _] _.
+Arguments T_q_etc [_ _] _ _.
 Arguments T_reind_id [_] _.
-Arguments T_q_id [_ _ _] _ _.
+Arguments T_q_id [_ _ _ _] _ _.
 Arguments T_reind_comp [_] _.
-Arguments T_q_comp [_ _ _] _ _.
+Arguments T_q_comp [_ _ _ _] _ _.
 
 (* Parallel the way a split type-cat is built up: *)
 
@@ -49,15 +47,15 @@ Definition struct1 : UU
 
 Definition struct2 (S1 : struct1)
   (ty := pr1 S1) (ext := pr1 (pr2 S1)) (reind := pr2 (pr2 S1))
-:= Σ dpr q dpr_q, @T_reind_pb ty ext reind dpr q dpr_q.
+:= Σ dpr, @T_q_etc ty ext dpr reind.
 
 Definition struct_total := Σ S1, struct2 S1.
 
 Definition is_split (S : struct_total)
-  (ty := pr1 (pr1 S)) (reind := pr2 (pr2 (pr1 S))) (q := pr1 (pr2 (pr2 S)))
+  (ty := pr1 (pr1 S)) (reind := pr2 (pr2 (pr1 S))) (q_etc := pr2 (pr2 S))
 := T_set ty
-  × (Σ reind_id, T_q_id q reind_id)
-  × (Σ reind_comp, T_q_comp q reind_comp).
+  × (Σ reind_id, T_q_id q_etc reind_id)
+  × (Σ reind_comp, T_q_comp q_etc reind_comp).
 
 Definition split_struct := Σ S, is_split S.
 
@@ -83,14 +81,14 @@ Definition obj_ext_struct
 Definition gen_q_mor_data (X : obj_ext_struct)
   (ext := pr1 (pr2 X)) (dpr := pr2 (pr2 X))
   (reind := pr2 (pr1 (pr1 X))) 
-:= Σ (q : T_q ext reind) (dpr_q : T_dpr_q dpr q), T_reind_pb dpr_q.
+:= T_q_etc dpr reind.
 
-Definition gen_q_mor_axs {X : obj_ext_struct} (Q : gen_q_mor_data X)
-  (q := pr1 Q) (A := pr2 (pr1 X)) (reind_id := pr1 A) (reind_comp := pr2 A)
-:= T_q_id q reind_id × T_q_comp q reind_comp.
+Definition gen_q_mor_axs {X : obj_ext_struct} (q_etc : gen_q_mor_data X)
+  (A := pr2 (pr1 X)) (reind_id := pr1 A) (reind_comp := pr2 A)
+:= T_q_id q_etc reind_id × T_q_comp q_etc reind_comp.
 
 Definition gen_q_mor_struct (X : obj_ext_struct)
-:= Σ (Q : gen_q_mor_data X), gen_q_mor_axs Q.
+:= Σ (q_etc : gen_q_mor_data X), gen_q_mor_axs q_etc.
 
 Definition reassoc_split_struct
 := Σ X, gen_q_mor_struct X.
@@ -168,42 +166,40 @@ Definition T_dpr
   := (λ ty ext, Π (Γ : CC) (A : ty Γ), ext Γ A --> Γ ).
 Definition T_reind
   := (λ ty, Π (Γ : CC) (A : ty Γ) (Γ' : CC), (Γ' --> Γ) -> ty Γ').
-Definition T_q
-  := (λ ty (ext : T_ext ty) (reind : T_reind ty), 
-       Π (Γ:CC) (A : ty Γ) Γ' (f : Γ' --> Γ),
-         (ext Γ' (reind _ A _ f)) --> (ext Γ A)).
-Definition T_dpr_q
-  := (λ ty ext reind (dpr : T_dpr ty ext) (q : T_q ty ext reind),
-       Π Γ (A : ty Γ) Γ' (f : Γ' --> Γ),
-         q _ A _ f ;; dpr Γ A = dpr Γ' (reind _ A _ f) ;; f).
-Definition T_reind_pb
-  := (λ ty ext reind dpr q (dpr_q : T_dpr_q ty ext reind dpr q),
-       Π Γ (A : ty Γ) Γ' (f : Γ' --> Γ),
-         isPullback _ _ _ _ (dpr_q _ A _ f)).
+Definition T_q_etc
+  := (λ ty ext (dpr : T_dpr ty ext) (reind : T_reind ty), 
+     Σ (q : Π (Γ:CC) (A : ty Γ) Γ' (f : Γ' --> Γ),
+         (ext Γ' (reind _ A _ f)) --> (ext Γ A))
+       (dpr_q : Π Γ (A : ty Γ) Γ' (f : Γ' --> Γ),
+         q _ A _ f ;; dpr Γ A = dpr Γ' (reind _ A _ f) ;; f),
+       (Π Γ (A : ty Γ) Γ' (f : Γ' --> Γ),
+         isPullback _ _ _ _ (dpr_q _ A _ f))).
 Definition T_set
   := (λ ty : T_ty, Π Γ, isaset (ty Γ)).
 Definition T_reind_id
   := (λ ty (reind : T_reind ty), Π Γ A, reind _ A _ (identity Γ) = A).
 Definition T_q_id
-  := (λ ty ext reind (q : T_q ty ext reind) (reind_id : T_reind_id ty reind),
-       Π Γ A, q _ A _ (identity Γ)
+  := (λ ty ext dpr reind (q_etc : T_q_etc ty ext dpr reind)
+                         (reind_id : T_reind_id ty reind),
+       Π Γ A, (pr1 q_etc) _ A _ (identity Γ)
               = idtoiso (maponpaths (ext Γ) (reind_id Γ A))).
 Definition T_reind_comp
   := (λ ty (reind : T_reind ty),
        Π Γ A Γ' (f : Γ' --> Γ) Γ'' (g : Γ'' --> Γ'),
          reind _ A _ (g ;; f) = reind _ (reind _ A _ f) _ g).
 Definition T_q_comp
-  := (λ ty ext reind (q : T_q ty ext reind) (reind_comp : T_reind_comp ty reind),
+  := (λ ty ext dpr reind (q_etc : T_q_etc ty ext dpr reind)
+                         (reind_comp : T_reind_comp ty reind),
        Π Γ A Γ' (f : Γ' --> Γ) Γ'' (g : Γ'' --> Γ'),
-         q _ A _ (g ;; f)
+         (pr1 q_etc) _ A _ (g ;; f)
          = idtoiso (maponpaths (ext Γ'') (reind_comp _ A _ f _ g))
-               ;; q _ (reind _ A _ f) _ g
-               ;; q _ A _ f).
+               ;; (pr1 q_etc) _ (reind _ A _ f) _ g
+               ;; (pr1 q_etc) _ A _ f).
 
 Definition eq_standalone_structural
   : split_type_struct CC
     = split_struct
-        T_ty T_ext T_dpr T_reind T_q T_dpr_q T_reind_pb
+        T_ty T_ext T_dpr T_reind T_q_etc
         T_set T_reind_id T_q_id T_reind_comp T_q_comp.
 Proof.
   Time apply idpath.
@@ -212,18 +208,18 @@ Time Defined.
 Definition weq_standalone_structural
   : split_type_struct CC
     ≃ split_struct
-        T_ty T_ext T_dpr T_reind T_q T_dpr_q T_reind_pb
+        T_ty T_ext T_dpr T_reind T_q_etc
         T_set T_reind_id T_q_id T_reind_comp T_q_comp.
 Proof.
   apply eqweqmap, eq_standalone_structural.
 Defined.
 
-(** Alternate construction of [weq_standalone_structural], retained in case they have better computational behaviour: *)
+(** Alternate construction of [weq_standalone_structural], retained in case it gives better computational behaviour: *)
 
 Definition standalone_to_structural
   : split_type_struct CC
     -> split_struct
-        T_ty T_ext T_dpr T_reind T_q T_dpr_q T_reind_pb
+        T_ty T_ext T_dpr T_reind T_q_etc
         T_set T_reind_id T_q_id T_reind_comp T_q_comp.
 Proof.
   intros S. exact (transportf (fun X => X) eq_standalone_structural S).
@@ -231,8 +227,8 @@ Defined.
 
 Definition structural_to_standalone
   : split_struct
-      T_ty T_ext T_dpr T_reind T_q T_dpr_q T_reind_pb
-      T_set T_reind_id T_q_id T_reind_comp T_q_comp
+        T_ty T_ext T_dpr T_reind T_q_etc
+        T_set T_reind_id T_q_id T_reind_comp T_q_comp
   -> split_type_struct CC.
 Proof.
   intros S. exact (transportb (fun X => X) eq_standalone_structural S).
@@ -241,7 +237,7 @@ Defined.
 Definition weq_standalone_structural_explicit
   : split_type_struct CC
     ≃ split_struct
-        T_ty T_ext T_dpr T_reind T_q T_dpr_q T_reind_pb
+        T_ty T_ext T_dpr T_reind T_q_etc
         T_set T_reind_id T_q_id T_reind_comp T_q_comp.
 Proof.
   refine (weqgradth
@@ -256,8 +252,7 @@ End Split_Type_Cat_as_Structural.
 
 (* TODO:
 
-- give weak equivalence between the second association of the abstracted version, and the imported version using object-extension structures
-- try condensing q, dpr_q, pb into a single item; see how that improves compilation time. *)
+- give weak equivalence between the second association of the abstracted version, and the imported version using object-extension structures *)
 
 
 
