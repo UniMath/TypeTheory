f7cc379b21bfb5fc89a3eb0b2ed5896d8ecdc14a
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2017-03-31 10:20:33 +0200

    added stuff to summary

--- TypeTheory/ALV1/Summary.v
+++ TypeTheory/ALV1/Summary.v
@@ -21,6 +21,7 @@ Require Import TypeTheory.Auxiliary.Auxiliary.
 Require Import TypeTheory.ALV1.RelativeUniverses.
 Require Import TypeTheory.ALV1.CwF_SplitTypeCat_Defs.
 Require Import TypeTheory.ALV1.RelUnivYonedaCompletion.
+Require Import TypeTheory.ALV1.CwF_SplitTypeCat_Maps.
 Require Import TypeTheory.ALV1.CwF_SplitTypeCat_Equivalence.
 Require Import TypeTheory.ALV1.CwF_SplitTypeCat_Cats_Standalone.
 Require Import TypeTheory.ALV1.CwF_def.
@@ -29,7 +30,51 @@ Require Import TypeTheory.ALV1.TypeCat_Reassoc.
 Require Import TypeTheory.ALV1.CwF_Defs_Equiv.
 Require Import TypeTheory.ALV1.RepMaps.
 
+
 (** * Equivalence of types between split type structures and families structures *)
+
+
+(** ** Equivalence between two versions of cwf structures *)
+
+Definition weq_cwf_cwf'_structure (C : Precategory)
+  : cwf_structure C ≃ cwf'_structure C.
+Proof.
+  exact (weq_cwf_cwf'_structure _ ).
+Defined.
+
+(** ** Equivalence between two versions of split typecat structures *)
+
+Definition weq_standalone_to_regrouped (C : Precategory)
+  : split_typecat_structure C ≃ split_typecat'_structure C.
+Proof.
+  exact weq_standalone_to_regrouped.
+Defined.
+
+(** ** Compatible [qq] from term structure and vice versa *)
+
+Definition compatible_qq_from_term {C : Precategory} 
+           {X : obj_ext_structure C} (Y : term_fun_structure C X)
+  : compatible_qq_morphism_structure Y.
+Proof.
+  apply compatible_qq_from_term.
+Defined.
+
+Definition compatible_term_from_qq {C : Precategory} 
+           {X : obj_ext_structure C} (Z : qq_morphism_structure X)
+  : compatible_term_structure Z.
+Proof.
+  apply compatible_term_from_qq.
+Defined.
+
+(** ** Auxiliary equivalence between the reordered structures *)
+
+Definition weq_cwf'_sty' (C : Precategory)
+  : cwf'_structure C ≃ split_typecat'_structure C.
+Proof.
+  apply weq_cwf'_sty'.
+Defined.
+
+(** ** Main construction: equivalence between split typecat structures and cwf structures *)
 
 Definition weq_sty_cwf (C : Precategory)
   : split_typecat_structure C ≃ cwf_structure C.
@@ -38,6 +83,10 @@ Proof.
   eapply weqcomp. eapply invweq. apply weq_cwf'_sty'.
   apply invweq, weq_cwf_cwf'_structure.
 Defined.
+
+
+
+
 
 (** * Map from [cwf_structure C] to [rep_map C] *)
 (** and proof that the map is an equivalence when [C] is univalent *)  
