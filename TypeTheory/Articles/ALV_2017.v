e9490d3324894c9be7b141795dd8e4934a7443c4
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2017-04-01 11:33:44 +0200

    added construction

--- TypeTheory/Articles/ALV_2017.v
+++ TypeTheory/Articles/ALV_2017.v
@@ -125,131 +125,21 @@ Proof.
   apply transfer_rep_map_weak_equiv.
 Defined.
 
-(** * Transfer of relative universe from Yoneda on a category to Yoneda on its Rezk completion *)
-Definition Rezk_on_RelUnivYoneda
-     : ∏ C : Precategory,
-       relative_universe (yoneda C (homset_property C))
-       → relative_universe
-           (yoneda (Rezk_completion C (homset_property C))
-              (homset_property (Rezk_completion C (homset_property C)))).
+(** * Equivalence between [rep_map C] and [cwf (Rezk_completion C)] *)
+
+Definition weq_rep_map_cwf_Rezk (C : Precategory)
+  : rep_map C ≃ cwf_structure (Rezk_completion C (homset_property _ ) ).
 Proof.
-  exact Rezk_on_RelUnivYoneda.
+  eapply weqcomp.
+    apply Rezk_on_rep_map.
+  apply invweq.
+  use (weqpair _ (isweq_from_cwf_to_rep _ _ )).
+  apply category_is_category.
 Defined.
-(* Print Assumptions Rezk_on_RelUnivYoneda. *)
-(**
-<<
-Axioms:
-univalenceAxiom : univalenceStatement
-isweqtoforallpathsAxiom : isweqtoforallpathsStatement
-funcontrAxiom : funcontrStatement
-Theory:
-Type hierarchy is collapsed (logic is inconsistent)
->>
-*)
-
-(** * Equivalence between type of CwF structures on [C] and of rel univs on [Yo C] *)
-Definition weq_cwf_structure_RelUnivYo
-     : ∏ C : Precategory, cwf_structure C ≃ @relative_universe C _ Yo.
-Proof.
-  exact weq_cwf_structure_RelUnivYo.
-Defined.
-(* Print Assumptions weq_cwf_structure_RelUnivYo. *)
-(** 
-<<
-Axioms:
-univalenceAxiom : univalenceStatement
-isweqtoforallpathsAxiom : isweqtoforallpathsStatement
-funcontrAxiom : funcontrStatement
-Theory:
-Type hierarchy is collapsed (logic is inconsistent)
->>
-*)
-
-(** * Transfer of CwF structure from a category to its Rezk completion*)
-Definition Rezk_on_cwf_structures (C : Precategory) 
-           (H : CwF_def.cwf_structure C) 
-  : CwF_def.cwf_structure (Rezk_completion C (homset_property _)) .
-Proof.
-  apply (invmap (weq_cwf_structure_RelUnivYo _)).
-  apply (Rezk_on_RelUnivYoneda C).
-  apply (weq_cwf_structure_RelUnivYo _).
-  exact H.
-Defined.
-(* Print Assumptions Rezk_on_cwf_structures. *)
-(** 
-<<
-Axioms:
-univalenceAxiom : univalenceStatement
-isweqtoforallpathsAxiom : isweqtoforallpathsStatement
-funextcontrAxiom : funextcontrStatement
-funcontrAxiom : funcontrStatement
-Theory:
-Type hierarchy is collapsed (logic is inconsistent)
->>
-*)
 
 
 
 
-(** * Equivalence of types between term structures and _q_-morphism structures *)
-Definition weq_term_fun_qq_morphisms_structures
-     : ∏ (C : Precategory) (X : obj_ext_structure C),
-       term_fun_structure C X ≃ qq_morphism_structure X.
-Proof.
-  exact @weq_term_qq.
-Defined.
-(* Print Assumptions weq_term_fun_qq_morphisms_structures. *)
-(** 
-<<
-Axioms:
-univalenceAxiom : univalenceStatement
-isweqtoforallpathsAxiom : isweqtoforallpathsStatement
-funcontrAxiom : funcontrStatement
-Theory:
-Type hierarchy is collapsed (logic is inconsistent)
->>
-*)
 
-(** * Equivalence of categories between term structures and _q_-morphism structures, over a fixed object extension structure *)
-Definition equiv_of_category_of_cwf_split_type_structures
-     : ∏ (C : Precategory) (X : obj_ext_structure C),
-       adj_equivalence_of_precats
-         (functor_composite
-            (right_adjoint
-               (Auxiliary.left_adj_from_adj_equiv
-                  (compat_structures_precategory C X)
-                  (term_fun_precategory C X)
-                  (compat_structures_pr1_functor C X) 
-                  (pr1_equiv C X))) (compat_structures_pr2_functor C X)).
-Proof.
-  exact equiv_of_structures.
-Defined.
-(* Print Assumptions equiv_of_category_of_cwf_split_type_structures. *)
-(** 
-<<
-Axioms:
-isweqtoforallpathsAxiom : isweqtoforallpathsStatement
-funcontrAxiom : funcontrStatement
-Theory:
-Type hierarchy is collapsed (logic is inconsistent)
->>
-*)
 
-(** * Equivalence of types between term structures and _q_-morphism structures, over a fixed object extension structures  *)
-Definition equiv_of_types_of_cwf_split_type_structures
-     : ∏ (C : Precategory) (X : obj_ext_structure C),
-       term_fun_precategory C X ≃ qq_structure_precategory C X.
-Proof.
-  exact equiv_of_types_of_structures.
-Defined.
-(* Print Assumptions equiv_of_types_of_cwf_split_type_structures. *)
-(** 
-<<
-Axioms:
-univalenceAxiom : univalenceStatement
-isweqtoforallpathsAxiom : isweqtoforallpathsStatement
-funcontrAxiom : funcontrStatement
-Theory:
-Type hierarchy is collapsed (logic is inconsistent)
->>
-*)
+
