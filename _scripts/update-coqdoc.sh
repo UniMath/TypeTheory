95948f44ea6534e005475e9b7706589b072c75ad
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2017-07-03 21:29:17 +0100

    Fixed some errors in the coqdoc update instructions.

--- _scripts/update-coqdoc.sh
+++ _scripts/update-coqdoc.sh
@@ -4,11 +4,12 @@ echo "\
 # branches are very difficult to make robust.)
 # 
 # To update coqdoc, or add it for a new tag, run something like the
-# following, adapted for the branch/tag you want:
+# following, with ‘master’ replaced by the branch/tag you want:
 
 git checkout master
 make html
 git checkout gh-pages
-mv html/* coqdoc/master/*
+mv html/* coqdoc/master/
 _scripts/coqdoc-to-jekyll.sh coqdoc/master
-git commit \"Updated coqdoc for master\""
+git commit -am 'Updated coqdoc for master'
+"
