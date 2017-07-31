63e4412fa561d71098892d0899d224b6def5a16f
Ralph Matthes <Ralph.Matthes@irit.fr>
2017-07-31 15:15:23 +0200

    add also the master file by VV for this directory
    only Require Export commands and no implementation

---
+++ TypeTheory/Bsystems/lBsystems.v
@@ -0,0 +1,58 @@
+(** * Definition of B-systems using the length function.
+
+Vladimir Voevodsky. 
+
+Started December 3, 2014. 
+
+A companion code to the paper "B-systems". 
+
+ *)
+
+
+
+Require Export TypeTheory.Csystems.prelim.
+
+Require Export TypeTheory.Csystems.lTowers.
+
+Require Export TypeTheory.Csystems.ltowers_over.
+
+Require Export TypeTheory.Csystems.hSet_ltowers. 
+
+
+
+Require Export TypeTheory.Bsystems.lB_carriers.
+
+Require Export TypeTheory.Bsystems.T_Tt.
+
+Require Export TypeTheory.Bsystems.S_St.
+
+Require Export TypeTheory.Bsystems.TS_ST.
+
+Require Export TypeTheory.Bsystems.STid .
+
+Require Export TypeTheory.Bsystems.dlt .
+
+Require Export TypeTheory.Bsystems.prelB_non_unital.
+
+Require Export TypeTheory.Bsystems.prelB .
+
+Require Export TypeTheory.Bsystems.lB0_non_unital .
+
+Require Export TypeTheory.Bsystems.lB0 .
+
+Require Export TypeTheory.Bsystems.lB_non_unital .
+
+Require Export TypeTheory.Bsystems.lB .
+
+Require Export TypeTheory.Bsystems.T_fun .
+
+Require Export TypeTheory.Bsystems.S_fun .
+
+Require Export TypeTheory.Bsystems.lB_to_precat.
+
+
+
+
+(* End of the file lBsystems.v *)
+
+
