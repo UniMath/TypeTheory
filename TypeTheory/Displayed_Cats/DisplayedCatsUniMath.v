1b9d2b1a9786666b7631d5b8d1b203715649f765
Ralph Matthes <Ralph.Matthes@irit.fr>
2017-10-02 18:52:13 +0200

    Require Import -> Require Export, following a suggestion by Benedikt

--- TypeTheory/Displayed_Cats/DisplayedCatsUniMath.v
+++ TypeTheory/Displayed_Cats/DisplayedCatsUniMath.v
@@ -1,17 +1,17 @@
 (** * Import the part now residing in UniMath *)
 
 
-Require Import UniMath.CategoryTheory.DisplayedCats.Auxiliary.
-Require Import UniMath.CategoryTheory.DisplayedCats.Core.
-Require Import UniMath.CategoryTheory.DisplayedCats.Constructions.
+Require Export UniMath.CategoryTheory.DisplayedCats.Auxiliary.
+Require Export UniMath.CategoryTheory.DisplayedCats.Core.
+Require Export UniMath.CategoryTheory.DisplayedCats.Constructions.
 
-Require Import UniMath.CategoryTheory.DisplayedCats.Fibrations.
-Require Import UniMath.CategoryTheory.DisplayedCats.Equivalences.
-Require Import UniMath.CategoryTheory.DisplayedCats.Equivalences_bis.
+Require Export UniMath.CategoryTheory.DisplayedCats.Fibrations.
+Require Export UniMath.CategoryTheory.DisplayedCats.Equivalences.
+Require Export UniMath.CategoryTheory.DisplayedCats.Equivalences_bis.
 
-Require Import UniMath.CategoryTheory.DisplayedCats.Codomain.
-Require Import UniMath.CategoryTheory.DisplayedCats.SIP.
-Require Import UniMath.CategoryTheory.DisplayedCats.Limits.
-Require Import UniMath.CategoryTheory.DisplayedCats.Examples.
+Require Export UniMath.CategoryTheory.DisplayedCats.Codomain.
+Require Export UniMath.CategoryTheory.DisplayedCats.SIP.
+Require Export UniMath.CategoryTheory.DisplayedCats.Limits.
+Require Export UniMath.CategoryTheory.DisplayedCats.Examples.
 
-Require Import UniMath.Topology.CategoryTop.
+Require Export UniMath.Topology.CategoryTop.
