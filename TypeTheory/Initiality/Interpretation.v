9dde61364aee74854711a00f92eeaa1b712c7398
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2018-10-23 11:49:36 +0200

    Fleshed out outline of interpretation

--- TypeTheory/Initiality/Interpretation.v
+++ TypeTheory/Initiality/Interpretation.v
@@ -10,6 +10,7 @@ Require Import TypeTheory.ALV1.TypeCat.
 Require Import TypeTheory.Initiality.SplitTypeCat_Maps.
 Require Import TypeTheory.Initiality.SplitTypeCat_Structure.
 Require Import TypeTheory.Initiality.Syntax.
+Require Import TypeTheory.Initiality.Typing.
 
 Section Auxiliary.
 
@@ -24,6 +25,16 @@ Section Auxiliary.
   (** This notation uses diagrammatic order, following UniMath’s notation
   [ (_ ∘ _)%Cat ] for morphisms in categories, _not_ consistent with
   UniMath’s notation [ (_ ∘ _)%functions ] for functions. *)
+
+  (* TODO: work out better way to treat this and the following *)
+  Definition mor_paths_hProp {C : category} {X Y : C} (f g : X --> Y)
+    : hProp
+  := hProppair (f = g) (homset_property C _ _ _ _).
+
+  (* TODO: work out better way to treat this *)
+  Definition type_paths_hProp {C : split_typecat} {Γ : C} (A B : C Γ)
+    : hProp
+  := hProppair (A = B) (isaset_types_typecat C _ _ _).
 
 End Auxiliary.
 
@@ -44,6 +55,12 @@ Section Environments.
     : term_with_type Γ -> term_with_type Γ'
   := fun a => ((type_of a)⦃f⦄,, reind_tm f a).
   
+  (*TODO: look for this upstream! *)
+  (** the “universal term of type A” *)
+  Definition var_typecat (Γ:C) (A:C Γ)
+  : tm (A ⦃dpr_typecat A⦄).
+  Admitted.
+
   (** An _environment_ over [Γ]: a map giving, for each variable from some potential context, a type and a term of that type.
 
   Motivating example: if [Γ] is the interpretation of some actual context, then each type of the context should be interpreted as some type over Γ, and each the corresponding variable can be extracted to a term of that type. *)
@@ -57,10 +74,10 @@ Section Environments.
   Proof.
     refine (dB_Sn_rect _ _ _).
     - exists (A ⦃dpr_typecat A⦄).
-      admit. (* the “universal term of type A” *)
+      apply var_typecat.
     - intro i.
       exact (reind_term_with_type (dpr_typecat A) (E i)).
-  Admitted.
+  Defined.
 
 End Environments.
 
@@ -78,7 +95,7 @@ Section Partial_Interpretation.
       {Γ:C} {n:nat} (E : environment Γ n) (A : C Γ) (e : tm_expr n)
     : partial (tm A).
   Proof.
-    (* TODO! *)
+    (* TODO: definition of the partial interpretation. *)
   Admitted.
   
 
@@ -89,3 +106,73 @@ Section Partial_Interpretation.
   I think either should work fine; I’m not sure which will work more cleanly. *)
 
 End Partial_Interpretation.
+
+Section Totality.
+
+  Context (C : split_typecat).
+
+  Definition environment_respects_type
+      {X : C} (Γ : context) (E : environment X Γ)
+    := forall i : Γ,
+      ∑ (d : is_defined (partial_interpretation_ty E (Γ i))),
+        (type_of (E i) = evaluate d).
+
+  Definition typed_environment (X : C) (Γ : context)
+    := ∑ (E : environment X Γ), environment_respects_type Γ E.
+
+  Coercion environment_of_typed_environment {X} {Γ}
+    (E : typed_environment X Γ) : environment X Γ
+  := pr1 E.
+
+  Definition typed_environment_respects_type {X} {Γ}
+    (E : typed_environment X Γ) (i : Γ)
+  := pr2 E.
+
+  Local Open Scope judgement_scope.
+
+  Definition is_interpretable (J : judgement) : hProp
+  := match J with
+     | [! |- Γ !] => htrue
+     | [! Γ |- A !]
+       => ∀ (X:C) (E : typed_environment X Γ),
+         is_defined (partial_interpretation_ty E A)         
+     | [! Γ |- A === A' !]
+       => ∀ (X:C) (E : typed_environment X Γ)
+            (d_A : is_defined (partial_interpretation_ty E A))   
+            (d_A' : is_defined (partial_interpretation_ty E A)),
+         type_paths_hProp (evaluate d_A) (evaluate d_A') 
+     | [! Γ |- a ::: A !]
+       => ∀ (X:C) (E : typed_environment X Γ)
+            (d_A : is_defined (partial_interpretation_ty E A)), 
+         is_defined (partial_interpretation_tm E (evaluate d_A) a)
+     | [! Γ |- a === a' ::: A !]
+       => ∀ (X:C) (E : typed_environment X Γ)
+          (d_A : is_defined (partial_interpretation_ty E A))
+          (d_a : is_defined (partial_interpretation_tm E (evaluate d_A) a)) 
+          (d_a' : is_defined (partial_interpretation_tm E (evaluate d_A) a)), 
+         mor_paths_hProp (evaluate d_a) (evaluate d_a')
+  end.
+  (* Note: we DON’T expect to need any inductive information for context judgements!
+
+  Essentially, our rules have been set up carefully enough that (hopefully) the context judgement could be removed entirely without loss. *)
+
+  Lemma is_interpretable_is_prop {J} : isaprop (is_interpretable J).
+  Proof.
+    destruct J; eauto using propproperty. 
+  Defined.
+
+  Fixpoint interpretable_from_derivation {J : judgement}
+    : derivation J -> is_interpretable J.
+  Proof.
+    (* TODO: totality of the interpretation.  This is the big one! *)
+  Admitted.
+
+  Lemma derivable_judgement_is_interpretable {J : judgement}
+    : ∥ derivation J ∥ -> is_interpretable J.
+  Proof.
+    apply factor_through_squash.
+    - apply is_interpretable_is_prop.
+    - apply interpretable_from_derivation.
+  Defined.
+
+End Totality.
