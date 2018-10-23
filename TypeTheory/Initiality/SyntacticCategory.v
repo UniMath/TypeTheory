0beae65e52ce6848bf1dd576190f541382447f74
Anders Mörtberg <andersmortberg@gmail.com>
2018-10-23 12:13:30 +0200

    prove a lemma about well-typed contexts being derivable

--- TypeTheory/Initiality/SyntacticCategory.v
+++ TypeTheory/Initiality/SyntacticCategory.v
@@ -11,23 +11,35 @@ Require Import TypeTheory.Initiality.SplitTypeCat_Structure.
 Require Import TypeTheory.Initiality.Syntax.
 Require Import TypeTheory.Initiality.Typing.
 
+Local Open Scope judgement.
+
 Section Types_and_Terms.
-  
-  Definition ty (Γ : context) : UU
-    := ∑ (A : ty_expr Γ), ∥ derivation [! Γ |- A !] ∥.
 
-  Coercion ty_expr_of_ty {Γ} (A : ty Γ) : ty_expr Γ := pr1 A.
+  Definition ty_with_derivation (Γ : context) : UU
+    := ∑ (A : ty_expr Γ), derivation [! Γ |- A !].
 
-  (* todo: [tm]! *)
+  Coercion ty_expr_of_ty_with_derivation {Γ} (A : ty_with_derivation Γ) :
+    ty_expr Γ := pr1 A.
+
+  Definition tm_with_derivation (Γ : context) (A : ty_expr Γ) : UU
+    := ∑ (a : tm_expr Γ), derivation [! Γ |- a ::: A !].
+
+  Coercion tm_expr_of_tm_with_derivation {Γ} {A : ty_with_derivation Γ}
+    (a : tm_with_derivation Γ A) : tm_expr Γ := pr1 a.
+
 End Types_and_Terms.
 
 Section Typed_Contexts.
 (** Here we define (well-typed) contexts, in the usual sense. *)
-(* TODO: perhaps rename [context] upstream to e.g. [raw_context] or [flat_context], so these can just be [context]? *)
 
-(* TODO: we could reasonably disentangle the “well-typed” aspect, in the sense of all the types being derivable types, and the “stratified” aspect. This shouldn’t be essential, but might make things clearer. *)
+(* TODO: perhaps rename [context] upstream to e.g. [raw_context] or
+[flat_context], so these can just be [context]? *)
 
-  Fixpoint typed_context_aux (n:nat)
+(* TODO: we could reasonably disentangle the “well-typed” aspect, in
+the sense of all the types being derivable types, and the “stratified”
+aspect. This shouldn’t be essential, but might make things clearer. *)
+
+  Fixpoint typed_context_aux (n : nat)
     : ∑ (X : Type), X -> context_of_length n.
   Proof.
     destruct n as [ | n].
@@ -35,12 +47,12 @@ Section Typed_Contexts.
       intros _; exact empty_context.
     - set (cxt_n := pr1 (typed_context_aux n)).
       set (realise := pr2 (typed_context_aux n) : cxt_n -> _).
-      exists (∑ (Γ : cxt_n), ty (realise Γ)).
+      exists (∑ (Γ : cxt_n), ty_with_derivation (realise Γ)).
       intros Γ_A.
       exact (context_extend (realise (pr1 Γ_A)) (pr2 Γ_A)).
   Defined.
 
-  Definition typed_context_of_length (n:nat)
+  Definition typed_context_of_length (n : nat)
   := pr1 (typed_context_aux n).
 
   Definition realise_typed_context {n}
@@ -51,7 +63,7 @@ Section Typed_Contexts.
     : typed_context_of_length >-> context_of_length.
 
   Definition typed_context : UU
-  := ∑ (n:nat), typed_context_of_length n.
+  := ∑ (n : nat), typed_context_of_length n.
 
   Definition typed_context_length : typed_context -> nat := pr1.
   Coercion typed_context_length : typed_context >-> nat.
@@ -63,15 +75,24 @@ Section Typed_Contexts.
     : typed_context_of_length n -> typed_context
   := fun Γ => (n,,Γ).
   Coercion make_typed_context : typed_context_of_length >-> typed_context.
- 
-  (* TODO: lemma: any well-typed context is indeed derivably a context. *)
+
+  (* any well-typed context is indeed derivably a context. *)
+  Lemma typed_context_derivable (Γ : typed_context) : derivation [! |- Γ !].
+  Proof.
+    destruct Γ as [n Γ].
+    induction n as [|n IHn].
+    - now apply derive_cxt_empty.
+    - destruct Γ as [Γ A].
+      now apply (derive_cxt_extend (realise_typed_context Γ) A (IHn _)), A.
+  Defined.
+
 End Typed_Contexts.
 
 Section Context_Maps.
   (* Maybe all this section can be inlined into section [Category] below, depending how long it turns out. *)
 
   (* TODO: define: context maps are raw context maps, plus suitable typing information (but truncate derivations!); then quotient by judgemental equality!
-  
+
   To behave well, they should be assumed to go between contexts that are at least well-typed (but “stratifiedness” shouldn’t really be needed). *)
 
   (* TODO: define identity context map, composition.
