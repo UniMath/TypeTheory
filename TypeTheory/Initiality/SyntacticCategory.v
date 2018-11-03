d18d12fc681ecdaddf0c4105a33a5b6c3e801c70
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2018-11-03 13:51:27 +0100

    Started defining composition of context maps

--- TypeTheory/Initiality/SyntacticCategory.v
+++ TypeTheory/Initiality/SyntacticCategory.v
@@ -2,9 +2,7 @@
 
 As a matter of organisation: all concrete lemmas involving derivations should live upstream in [TypingLemmas]; this file should simply package them up into the appropriate categorical structure. *)
 
-Require Import UniMath.Foundations.All.
-Require Import UniMath.MoreFoundations.All.
-Require Import UniMath.CategoryTheory.All.
+Require Import UniMath.All.
 
 Require Import TypeTheory.ALV1.TypeCat.
 Require Import TypeTheory.Initiality.SplitTypeCat_Maps.
@@ -20,7 +18,66 @@ Section Auxiliary.
  (* we’ll need some material here about quotients:
   particularly, [lemmas.setquotprpathsandR] from [PAdics], I guess? *)
 
+  (* Upstream issues to possibly raise about [setquot]:
+  - should [pr1] of [eqrel] coerce to [hrel], not directly to [Funclass]? 
+  - should [QuotientSet.setquotfun2] replace [setquotfun2]? *)
+
+  (** Variant of [setquotuniv] with the [isaset] hypothesis separated out,
+  for easier interactive use with [use], analogous to [setquotunivprop']. *)
+  Definition setquotuniv' {X : UU} {R : hrel X} {Y : UU}
+      (isaset_Y : isaset Y) (f : X -> Y) (f_respects_R : iscomprelfun R f)
+    : setquot R -> Y.
+  Proof.
+    use (setquotuniv _ (_,,_)); assumption.
+  Defined.
+
+  (** TODO: is [setquot_rect], the general dependent universal property of [setquot], really not provided in the library somewhere?
+
+  Here we put it together from [setquotuniv] (the non-dependent version) and [setquotunivprop] (the version for hprop-valued predicates). Unfortunately, that means it doesn’t compute, since [setquotunivprop] doesn’t.  With a bit more thought, could we give a version that computes nicely like [setquotuniv]? *)
+  Definition setquot_rect_aux {X:UU} {R:eqrel X}
+      (P : setquot R -> UU) (isaset_P : forall xx, isaset (P xx))
+      (d : forall x:X, P (setquotpr R x))
+      (d_respects_R : forall (x y:X) (r : R x y),
+          transportf _ (iscompsetquotpr _ _ _ r) (d x) = d y)
+    : ∑ (f : forall xx, P xx), forall x, f (setquotpr R x) = d x.
+  Proof.
+    transparent assert (f_nondep : (setquot R -> ∑ xx, P xx)).
+    { use setquotuniv'.
+      - apply isaset_total2; try assumption. apply isasetsetquot. 
+      - intros x. exact (_,, d x).
+      - intros x y r. refine (total2_paths_f _ _). apply (d_respects_R _ _ r).
+    }
+    transparent assert (f_commutes : (forall xx, pr1 (f_nondep xx) = xx)).
+    { use setquotunivprop'.
+      - intros; apply isasetsetquot.
+      - intros x. apply idpath.
+    }
+    exists (fun xx => transportf _ (f_commutes xx) (pr2 (f_nondep xx))).
+    intros x. eapply pathscomp0. 2: { apply idpath_transportf. }
+    apply maponpaths_2. apply isasetsetquot.
+  Qed.
+
+  Definition setquot_rect {X:UU} {R:eqrel X}
+      (P : setquot R -> UU) (isaset_P : forall xx, isaset (P xx))
+      (d : forall x:X, P (setquotpr R x))
+      (d_respects_R : forall (x y:X) (r : R x y),
+          transportf _ (iscompsetquotpr _ _ _ r) (d x) = d y)
+    : forall xx, P xx
+  := pr1 (setquot_rect_aux P isaset_P d d_respects_R).
+
+  Definition setquot_comp {X:UU} {R:eqrel X}
+      (P : setquot R -> UU) (isaset_P : forall xx, isaset (P xx))
+      (d : forall x:X, P (setquotpr R x))
+      (d_respects_R : forall (x y:X) (r : R x y),
+          transportf _ (iscompsetquotpr _ _ _ r) (d x) = d y)
+    : forall x, (setquot_rect P isaset_P d d_respects_R) (setquotpr R x) = d x
+  := pr2 (setquot_rect_aux P isaset_P d d_respects_R).
+
 End Auxiliary.
+
+(** The construction of the syntactic type-category is rather trickier than one might hope, because of the need to quotient by some form of context equality — which, as ever when quotienting objects of a category, is quite fiddly.
+
+For just the _category_ this is unnecessary, but for the _type-category_, it is unavoidable: types must be modulo equality, in order to form a presheaf, but then so must contexts, in order for context extension to be well-defined. *)
 
 Section Context_Equality.
 (* Probably this (or some of it) should be upstreamed to with the other “auxiliary judgements” in [TypingLemmas]. *)
@@ -36,12 +93,14 @@ Section Context_Equality.
 
   Two contexts are equal if they _both_ believe all their types are equal.
 
-  (Note: one direction wouldn’t suffice, for general type theories.  E.g.
+  Note 1: one direction wouldn’t suffice, for general type theories.  E.g.
   in ETT with the reflection rule, define a predicate [P] over [nat] with
   [ P 0 ] false, and [ P 1 ] true.  Then [ P 0 ] proves that [ 0 === 1 : nat ]
   and hence that [ P 0 === P 1 ], but [ P 1 ] doesn’t prove this, so for the
   contexts [ x0 : P 0 ] and [ x0 : P 1 ], one direction of the below conditions
-  will hold, but not both. *)
+  will hold, but not both.
+
+  Note 2: this is a _flat_ form of context equality, not stratified, analogous to [ [! |f- Γ !] ] not [ [! |f- Γ !] ].  As such, we don’t expect it to give a contextual category: a context may (up to equality) be built up from types in several different ways.  For initiality, we will at some point have to remedy this: either by taking the contextual core of what this gives, or some other way. *)
   Definition derivation_cxteq {n} (Γ Δ : context_of_length n) : UU
   :=   (forall i, [! Γ |- Γ i === Δ i !])
      × (forall i, [! Δ |- Δ i === Γ i !]).
@@ -61,7 +120,6 @@ Section Context_Equality.
   Definition derivable_cxteq {n} : eqrel (wellformed_context_of_length n)
   := (_,,derivable_cxteq_is_eqrel n).
 
-  (* TODO: raise issue upstream: shoud [pr1] of [eqrel] coerce to [hrel], not directly to [Funclass]? *)
 End Context_Equality.
 
 Section Contexts_Modulo_Equality.
@@ -91,18 +149,21 @@ End Contexts_Modulo_Equality.
 
 Section Context_Maps.
 
+  (** Note: the truncation of the derivation part is mathematically redundant,
+  since we will later quotient anyway.  However, it lets us get better
+  computational behaviour on the map part, in compositions etc. *)
   Local Definition map (ΓΓ ΔΔ : context_mod_eq) : UU
     := ∑ (f : raw_context_map (length ΓΓ) (length ΔΔ)),
-       forall (Γ : ΓΓ) (Δ : ΔΔ), [! |- f ::: Γ ---> Δ !].
+       ∥ forall (Γ : ΓΓ) (Δ : ΔΔ), [! |- f ::: Γ ---> Δ !] ∥.
 
   (* TODO: lemma that here (and in later similar definitions) it’s sufficient to show typing for _some_ representative, to get it for all representatives. *)
 
-  Coercion raw_of_context_map {ΓΓ ΔΔ} (f : map ΓΓ ΔΔ)
-    : raw_context_map _ _
+  Definition raw_of_context_map {ΓΓ ΔΔ} (f : map ΓΓ ΔΔ) : raw_context_map _ _
   := pr1 f.
+  Coercion raw_of_context_map : map >-> raw_context_map.
 
-  Local Definition derivation_of_map {ΓΓ ΔΔ} (f : map ΓΓ ΔΔ)
-    : forall (Γ : ΓΓ) (Δ : ΔΔ), [! |- f ::: Γ ---> Δ !]
+  Local Definition map_derivable {ΓΓ ΔΔ} (f : map ΓΓ ΔΔ)
+    : ∥ forall (Γ : ΓΓ) (Δ : ΔΔ), [! |- f ::: Γ ---> Δ !] ∥
   := pr2 f.
   
   Local Definition mapeq_hrel {ΓΓ ΔΔ} (f g : map ΓΓ ΔΔ)
@@ -127,6 +188,40 @@ Section Context_Maps.
     : map ΓΓ ΔΔ
   := pr1 f.
   Coercion map_representative_as_map : map_representative >-> map.
+
+  (* TODO: upsteam *)
+  Lemma take_context_representative
+      (ΓΓ : context_mod_eq) {X:UU} (h_X : isaprop X)
+      (f : context_representative ΓΓ -> X)
+    : X.
+  Proof.
+    (* use surjectivity *)
+  Admitted.
+
+  Local Definition compose
+      {ΓΓ ΔΔ ΘΘ} (ff : map_mod_eq ΓΓ ΔΔ) (gg : map_mod_eq ΔΔ ΘΘ)
+    : map_mod_eq ΓΓ ΘΘ.
+  Proof.
+    revert ff gg. use QuotientSet.setquotfun2.
+    - intros f g. exists (comp_raw_context f g).
+      (* destruct ΓΓ as [n ΓΓ]; revert ΓΓ f g.
+      (* TODO: abstract this universal property better. *)
+      refine (setquotunivprop' _ _ _).
+      { eauto using isapropishinh, impred_isaprop. }
+      intros Γ f g. *)
+      apply (squash_to_prop (map_derivable f)). { apply isapropishinh. }
+      intros d_f.
+      apply (squash_to_prop (map_derivable g)). { apply isapropishinh. }
+      intros d_g.
+      apply (take_context_representative ΔΔ). { apply isapropishinh. }
+      intros Δ.
+      apply hinhpr; intros Γ Θ.
+      refine (@derivation_comp_raw_context Γ Δ Θ _ (raw_of_context_map f) _ _ _);
+        auto.
+      exact (pr2 (Γ : wellformed_context_of_length _)).
+        (* TODO: access function! *)
+    - admit.
+  Admitted.
 
   (* TODO: define identity context map, composition.
 
