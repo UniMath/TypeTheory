8bfb82a9d8c750a6e2e444f031ac88d676d54f20
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2018-12-05 18:24:54 +0100

    Gave interpretation of strat cxts

--- TypeTheory/Initiality/Initiality.v
+++ TypeTheory/Initiality/Initiality.v
@@ -7,9 +7,11 @@ Require Import UniMath.CategoryTheory.All.
 
 
 Require Import TypeTheory.Auxiliary.Auxiliary.
+Require Import TypeTheory.Auxiliary.Partial.
 Require Import TypeTheory.ALV1.TypeCat.
 Require Import TypeTheory.Initiality.SplitTypeCat_Structure.
 Require Import TypeTheory.Initiality.SplitTypeCat_Maps.
+Require Import TypeTheory.Initiality.Typing.
 Require Import TypeTheory.Initiality.SyntacticCategory.
 Require Import TypeTheory.Initiality.Interpretation.
 
@@ -173,6 +175,95 @@ Note that such a base object is necessarily unique: see [isaprop_is_contextual].
   Qed.
 
 End Contextual_Cats.
+
+Section Interpretation_Stratified_Contexts.
+(* TODO: upstream to [InterpretationLemmas]? *)
+
+  Context {C : split_typecat}
+          (U : universe_struct C)
+          (Π : pi_struct C)
+          (X : C).
+
+  (* TODO: upstream *)
+  Definition empty_environment
+    : environment X 0.
+  Proof.
+    intros [].
+  Defined.
+
+  (* TODO: refactor this!
+     Show separately:
+     - an extension carries a canonical environment;
+     - a strat cxt interprets as a partial extension *)
+  Definition partial_interpretation_strat_cxt {n}
+      (Γ : stratified_context_of_length n)
+    : partial (∑ Y : C, environment Y n).
+  Proof.
+    induction n as [ | n' IH].
+    - apply return_partial.
+      exists X. exact empty_environment.
+    - apply (bind_partial (IH (context_rest Γ))).
+      intros [interp_Γ E].
+      apply (bind_partial
+              (partial_interpretation_ty U Π E (context_last Γ))).
+      intros interp_A.
+      apply return_partial.
+      exists (interp_Γ ◂ interp_A).
+      apply extend_environment, E.
+  Defined.
+  Arguments partial_interpretation_strat_cxt : simpl nomatch.
+
+  (* TODO: upstream *)
+  Definition isaprop_environment_respects_type {Y}
+      (Γ : context) (E : environment Y Γ)
+    : isaprop (environment_respects_type U Π Γ E).
+  Proof.
+  Admitted.
+
+  Definition strat_cxt_partial_interpretation_respects_type {n}
+      (Γ : stratified_context_of_length n)
+      (Γ_def : is_defined (partial_interpretation_strat_cxt Γ))
+    : environment_respects_type U Π Γ (pr2 (evaluate Γ_def)).
+  Proof.
+    induction n as [ | n' IH].
+    - intros [].
+    (* TODO: perhaps factor out [empty_environment_respects_type]. *)
+    - destruct Γ_def as [Γ'_def [A_def []]]; cbn in Γ'_def, A_def |- *.
+      refine (extend_environment_respects_type _ _ (IH _ Γ'_def) _).
+  Qed.
+
+  Definition derivable_strat_cxt_is_interpretable {n}
+      (Γ : stratified_context_of_length n)
+      (d_Γ : ∥ [! |- Γ !] ∥)
+    : is_defined (partial_interpretation_strat_cxt Γ).
+  Proof.
+    refine (factor_through_squash _ _ d_Γ). { apply propproperty. }
+    clear d_Γ; intros d_Γ.
+    induction n as [ | n' IH].
+    - constructor.
+    - destruct d_Γ as [d_Γ' d_A].
+      specialize (IH _ d_Γ').
+      exists IH. cbn.
+      refine (_,,tt).
+      refine (@derivable_judgement_is_interpretable _ U Π
+                               [! context_rest Γ |- _ !] _ _ (_,,_));
+        auto using hinhpr.
+      apply strat_cxt_partial_interpretation_respects_type.
+  Qed.
+
+  Definition derivable_strat_cxteq_is_interpretable {n}
+      (Γ Γ' : stratified_context_of_length n)
+      (e_Γ : ∥ [! |- Γ === Γ' !] ∥)
+      (Γ_def : is_defined (partial_interpretation_strat_cxt Γ))
+      (Γ'_def : is_defined (partial_interpretation_strat_cxt Γ'))
+    : evaluate Γ_def = evaluate Γ'_def.
+  Proof.
+    refine (factor_through_squash _ _ e_Γ);
+    admit. (* Requires refactoring of [partial_interpretation_strat_cxt]
+            to give extension instead of object. *)
+  Admitted.
+
+End Interpretation_Stratified_Contexts.
 
 Section Existence.
 
