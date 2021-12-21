3147a17949ed62427a4124786f32674e9cb6731d
Peter LeFanu Lumsdaine <p.l.lumsdaine@gmail.com>
2021-12-21 22:01:01 +0000

    Resurrected CwF_Pitts_natural

--- TypeTheory/OtherDefs/CwF_Pitts_natural.v
+++ TypeTheory/OtherDefs/CwF_Pitts_natural.v
@@ -13,15 +13,14 @@
   http://www.cl.cam.ac.uk/~amp12/papers/nompcs/nompcs.pdf (page=9)
 *)
 
-Require Import Systems.Auxiliary.
-Require Import Systems.UnicodeNotations.
-Require Import UniMath.Foundations.Sets.
-Require Import UniMath.CategoryTheory.functor_categories.
-Require Import UniMath.CategoryTheory.category_hset.
-Require Import UniMath.CategoryTheory.opp_precat.
-Require Import UniMath.CategoryTheory.yoneda.
-Require Import UniMath.CategoryTheory.UnicodeNotations.
+Require Import UniMath.Foundations.All.
+
+Require Import TypeTheory.Auxiliary.CategoryTheoryImports.
+
+Require Import UniMath.CategoryTheory.FunctorCategory.
+Require Import UniMath.CategoryTheory.categories.HSET.All.
 Require Import UniMath.CategoryTheory.limits.pullbacks.
+Require Import TypeTheory.Auxiliary.Auxiliary.
 
 
 Local Notation "# F" := (functor_on_morphisms F)(at level 3).
@@ -31,8 +30,7 @@ Local Notation "C '^op'" := (opp_precat C) (at level 3, format "C ^op").
 
 Module Preview.
   
-Variable C : precategory.
-Variable hs : has_homsets C. 
+Variable C : category.
 Variable Ty Tm: [C^op, HSET]. (* functor C^op HSET. *)
 Variable p : _ ⟦Tm, Ty⟧. (* needs to be written as mor in a precat *)
 
@@ -44,7 +42,7 @@ Variable q : forall (Γ : C) (A : pr1hSet ((Ty : functor _ _)  Γ)),
 Definition yoTy (Γ : C) :
   pr1hSet ((Ty : functor _ _ ) Γ)
           ≃
-  _ ⟦(yoneda C hs Γ) , Ty ⟧.
+  _ ⟦(yoneda C Γ) , Ty ⟧.
 Proof.
   apply invweq.
   apply yoneda_weq.
@@ -53,7 +51,7 @@ Defined.
 Definition yoTm (Γ : C) :
   pr1hSet ((Tm : functor _ _ ) Γ)
           ≃
-  _ ⟦yoneda C hs Γ, Tm⟧.
+  _ ⟦yoneda C Γ, Tm⟧.
 Proof.
   apply invweq.
   apply yoneda_weq.
@@ -62,18 +60,17 @@ Defined.
 Variable comp_comm :
   forall Γ (A : pr1hSet ((Ty : functor _ _ ) Γ)),
     yoTm _ (q Γ A) ;; p =
-    #(yoneda _ _ ) (pi Γ A) ;; yoTy _ A.
+    #(yoneda _ ) (pi Γ A) ;; yoTy _ A.
 
 Variable ispullback_comp_comm : 
   forall Γ (A : pr1hSet ((Ty : functor _ _ ) Γ)),
-    isPullback _ _ _ _ _ (comp_comm Γ A).
+    isPullback (comp_comm Γ A).
 
 End Preview.
 
 Section definition.
 
-Variable C : precategory.
-Variable hsC : has_homsets C.
+Variable C : category.
 
 Definition tt_structure : UU :=
   ∑ TyTm : [C^op, HSET] × [C^op, HSET],
@@ -84,7 +81,7 @@ Definition Tm (X : tt_structure) : [C^op, HSET] := pr2 (pr1 X).
 Definition p (X : tt_structure) :  _ ⟦Tm X, Ty X⟧ := pr2 X.
 
 Definition comp_structure (X : tt_structure) : UU :=
-  ∀ Γ (A : pr1hSet ((Ty X : functor _ _ ) Γ)),
+  forall Γ (A : pr1hSet ((Ty X : functor _ _ ) Γ)),
     ∑ (comp : C) (pi : _ ⟦comp, Γ⟧), 
          pr1hSet ((Tm X : functor _ _ ) (comp)).
 
@@ -104,21 +101,21 @@ Definition q {X : tt_structure} (Y : comp_structure X) :
 Definition pullback_structure {X : tt_structure} (Y : comp_structure X) : UU
   := forall Γ (A : pr1hSet ((Ty X : functor _ _ ) Γ)),
        ∑ H : 
-          invmap (yoneda_weq _ _ _ (Tm X)) (q Y Γ A) ;; p X 
+          invmap (yoneda_weq C _ (Tm X)) (q Y Γ A) ;; p X 
           =
-          #(yoneda _ _ ) (pi Y Γ A) ;; invmap (yoneda_weq _ hsC _ (Ty X)) A , 
-         isPullback _ _ _ _ _ H.
+          #(yoneda _ ) (pi Y Γ A) ;; invmap (yoneda_weq _ _ (Ty X)) A , 
+         isPullback H.
 
 Definition comp_comm {X : tt_structure } {Y : comp_structure X} (Z : pullback_structure Y) 
   : forall Γ (A : pr1hSet ((Ty X : functor _ _ ) Γ)),
-      invmap (yoneda_weq _ _ _ (Tm X)) (q Y Γ A) ;; p X 
+      invmap (yoneda_weq _ _ (Tm X)) (q Y Γ A) ;; p X 
           =
-          #(yoneda _ _ ) (pi Y Γ A) ;; invmap (yoneda_weq _ hsC _ (Ty X)) A
+          #(yoneda _ ) (pi Y Γ A) ;; invmap (yoneda_weq _ _ (Ty X)) A
   := fun Γ A => pr1 (Z Γ A).
 
 Definition is_Pullback_comp {X : tt_structure } {Y : comp_structure X} (Z : pullback_structure Y) 
   : forall Γ (A : pr1hSet ((Ty X : functor _ _ ) Γ)),
-      isPullback _ _ _ _ _ (comp_comm Z _ _ ) 
+      isPullback (comp_comm Z _ _ ) 
   := fun Γ A => pr2 (Z Γ A).
 
 Definition is_natural_CwF : UU 
